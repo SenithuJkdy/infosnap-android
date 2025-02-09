87a95c238eb2bc16a7c7a4ba0a883ecde1acf75a
senithujkdy <senithujayakody1@gmail.com>
2025-02-09 22:05:07 +0530

    refactor(api): vercel integration

--- app/src/main/java/com/example/reporter_news_app/ui/Constants.kt
+++ app/src/main/java/com/example/reporter_news_app/ui/Constants.kt
@@ -2,5 +2,8 @@ package com.example.reporter_news_app.ui
 
 object Constants {
 //    const val BASE_URL = "http://192.168.1.100:5001"
-    const val BASE_URL = "http://192.168.45.166:5001"
+//    const val BASE_URL = "http://192.168.45.166:5001"
+//    const val BASE_URL = "https://potential-winner-5g45rp7x4qrw37qpj-5001.app.github.dev"
+    const val BASE_URL = "https://android-server-nine.vercel.app"
+
 }
